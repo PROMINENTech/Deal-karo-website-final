1f6852f651f0d95156c97c05eea4ed4ee5161cdf
Bikash Kumar Rathore <bikashkumarrathore141@gmail.com>
2024-11-04 20:18:02 +0530

    tracked all the files

---
+++ PY/Website.py
@@ -0,0 +1,10 @@
+print("Welcome to the deal karo website","indias no.1 selling website")
+name = "bikash kumar rathore"  #string
+age = 23
+Price = 5000
+print(name)
+print("my name is this", name)
+a=5
+b=6
+sum=a+b
+print(sum)
